b0bd6fe5bc025b973edf0d2f9b298f7b5cf49360
alejandrossm <alejandrossm@outlook.com>
2021-08-26 13:50:36 -0400

    Agregadas página 2 y 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+/*comentario de bloque*/
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'home',//comentario
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'pag2',
+    loadChildren: () => import('./pages/pag2/pag2.module').then( m => m.Pag2PageModule)
+  },
+  {
+    path: 'pag3',
+    loadChildren: () => import('./pages/pag3/pag3.module').then( m => m.Pag3PageModule)
   },
 ];
 
